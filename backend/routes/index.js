3fed59d44335cf90ffdcb4c0e43e653adbd0341b
Zach Wood <zacharydavwood@gmail.com>
2024-02-21 17:15:52 -0500

    "Added an API router"

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,10 @@
 // backend/routes/index.js
 const express = require('express'); // we require express 
 const router = express.Router();
+const apiRouter = require('./api'); // we require our api 
+
+
+router.use('/api', apiRouter); // we use our apiRouter and we provicde a oat 
 
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
