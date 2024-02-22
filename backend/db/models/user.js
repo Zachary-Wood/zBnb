3da0c85129a6bbce30c74968b470484a3eda19f2
Zach Wood <zacharydavwood@gmail.com>
2024-02-22 17:10:32 -0500

    Added User scope for user security and web safety

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -44,6 +44,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+      }
+    }
   });
   return User;
 };
