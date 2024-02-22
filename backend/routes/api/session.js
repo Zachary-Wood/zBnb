ba2232268dfad12800d9107e9c6f5f3b7f0038d7
Zach Wood <zacharydavwood@gmail.com>
2024-02-22 18:56:13 -0500

    Added a backend endpoint to get the current users session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -54,6 +54,24 @@ router.post(
       return res.json({ message: 'success' });
     }
   );
+
+
+  router.get(
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
+    }
+  );
   
   
 module.exports = router;
