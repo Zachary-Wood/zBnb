bed00b419685caa409f5fa5f1a281cac07ff0dd0
Zach Wood <zacharydavwood@gmail.com>
2024-02-22 18:49:28 -0500

    Add user logout backend end point

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -45,6 +45,15 @@ router.post(
       });
     }
   );
+
+
+  router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
   
   
 module.exports = router;
