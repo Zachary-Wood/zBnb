9c5b4c69b1295d802c6238583d96d7521551afbb
Zach Wood <zacharydavwood@gmail.com>
2024-02-23 13:00:42 -0500

    Added final fix for render

--- backend/db/migrations/20240222212310-create-user.js
+++ backend/db/migrations/20240222212310-create-user.js
@@ -48,7 +48,7 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIME TAMP') 
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') 
       }
     }, options);
   },
