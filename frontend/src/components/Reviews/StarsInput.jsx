1c3eec36d19b329c31f1f5c470b85a4658bf00c1
Zach Wood <zacharydavwood@gmail.com>
2024-04-25 16:53:43 -0400

    Changes for render 4

--- frontend/src/components/Reviews/StarsInput.jsx
+++ frontend/src/components/Reviews/StarsInput.jsx
@@ -1,23 +1,30 @@
-import {useState} from "react";
+import { useState } from "react";
 
-const StarInput = ({stars, setStars}) => {
-    const [starEffect, setStarEffect] = useState(stars);
-    const starRating = [1, 2, 3, 4, 5];
-    return(<div className="stars-rating">
-        <ul>
+const StarInput = ({ stars, setStars }) => {
+  const [starEffect, setStarEffect] = useState(stars);
+  const starRating = [1, 2, 3, 4, 5];
+  return (
+    <div className="stars-rating">
+      
+      
+      <ul>
 
-            {starRating.map(rating => {
-                return(<i 
-                    key={`stars-rating-${rating}`}
-                    className={`fa-${starEffect >= rating ? "solid" : "regular"} fa-star`}
-                    onClick={()=> setStars(starEffect)}
-                    onMouseEnter={()=> setStarEffect(rating)}
-                    onMouseLeave={()=> setStarEffect(stars)} />)
-            })}
-            Stars
-        </ul>
-        
-    </div>)
-}
+        {starRating.map((rating) => {
+          return (
+            <i
+              key={`${stars}-${rating}`}
+              className={`fa-${stars >= rating ? "solid" : "regular"} fa-star`}
+              onClick={() => setStars(rating)}
+              onMouseEnter={() => setStarEffect(rating)}
+              onMouseLeave={() => setStarEffect(stars)}
+            />
+          );
+        })}
+      Stars
+      
+      </ul>
+    </div>
+  );
+};
 
 export default StarInput;
